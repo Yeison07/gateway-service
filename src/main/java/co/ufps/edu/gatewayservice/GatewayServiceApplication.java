99265442f07442264ddad9a08fe890cc4e7b74cd
Yeison07 <yeisona07@hotmail.com>
2023-05-16 14:40:34 -0500

    crossorigin

--- src/main/java/co/ufps/edu/gatewayservice/GatewayServiceApplication.java
+++ src/main/java/co/ufps/edu/gatewayservice/GatewayServiceApplication.java
@@ -2,8 +2,10 @@ package co.ufps.edu.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
+@CrossOrigin
 public class GatewayServiceApplication {
 
     public static void main(String[] args) {
